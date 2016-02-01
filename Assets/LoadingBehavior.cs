a73c982ad767106f168a954f08fe86a4829fb3d3
alquist2 <mikea12122@gmail.com>
2016-01-31 21:49:07 -0500

    Updated Main Menu, added pause menu functionality, and started on a loading screen

---
+++ Assets/LoadingBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingBehavior : MonoBehaviour {
+
+    public Canvas[] loadingView;
+
+	// Use this for initialization
+	void Start ()
+    {
+        loadingView[1].enabled = false;
+        InvokeRepeating("ToggleViews", 4, 4);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //InvokeRepeating("ToggleViews", 4, 4);
+	}
+
+    void ToggleViews()
+    {
+        if (loadingView[0].enabled)
+        {
+            loadingView[0].enabled = false;
+            loadingView[1].enabled = true;
+        } else
+        {
+            loadingView[0].enabled = true;
+            loadingView[1].enabled = false;
+        }
+    }
+}
