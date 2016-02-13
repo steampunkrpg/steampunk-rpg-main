e50364f4849c028f246d3e2e48eabb20e4017139
Nefari0uss <nnefari0uss@gmail.com>
2016-02-13 17:27:47 -0500

    movement kinda works.

--- Assets/Scripts/Grid/GridCell.cs
+++ Assets/Scripts/Grid/GridCell.cs
@@ -5,18 +5,20 @@ public class GridCell : MonoBehaviour {
 
     public int col, row;
 
-    Vector2[][] directions = new Vector2[][]
-    {
-        // directions 0,     1,     2,      3,      4,   5
-        //            NE,    N,     NW,     SW,     S,   SE
-        new Vector2[] { new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, -1), new Vector2(-1, 0), new Vector2(-1, 1), new Vector2(0, 1) },
-        new Vector2[] { new Vector2(1, 0), new Vector2(1, -1), new Vector2(0, -1), new Vector2(-1, 0), new Vector2(0, 1), new Vector2(1, 1) }
-    };
+    Vector2[][] directions;
+
+
+    //
 
 
     void Start()
     {
-
+        directions = new Vector2[][] {
+            // directions           0,               1,                  2,                    3,                4,                    5
+            //                      NE,              N,                  NW,                   SW,               S,                    SE
+            new Vector2[] { new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, -1), new Vector2(-1, 0), new Vector2(-1, 1), new Vector2(0, 1) },
+            new Vector2[] { new Vector2(1, 0), new Vector2(1, -1), new Vector2(0, -1), new Vector2(-1, 0), new Vector2(0, 1), new Vector2(1, 1) }
+       };
     }
 
     void Update()
@@ -40,7 +42,10 @@ public class GridCell : MonoBehaviour {
     public GridCell GetNeighbor(int direction)
     {
         var parity = row & 1;
-        
+        Debug.Log("Parity: " + parity);
+
+        Debug.Log(directions.ToString());
+        //Debug.Log("Dir: " + directions[parity][0]);
         Vector2 dir = directions[parity][direction];
         GridCell gridCell = GameObject.Find("Hex " + (int)(row + dir.x) + ", " + (int)(row + dir.y)).GetComponent<GridCell>() as GridCell;
         return gridCell;
