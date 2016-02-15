126fff8e8490ceec3a819f493c07049a14147e7f
crunderwood <calebunderwood@gmail.com>
2016-02-15 15:40:03 -0500

    Added World Map
    World Map Scene

---
+++ Assets/Scripts/Character2HeightmapPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	private GameObject[] GOs;
+
+	// Use this for initialization
+	void Start () {
+		GOs = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+	}
+
+	void FixedUpdate () {
+		foreach (GameObject o in GOs) {
+			if (o.tag == "Unit" || o.tag == "Enemy"){
+				Vector3 pos = o.transform.position;
+				pos.y = Terrain.activeTerrain.SampleHeight(o.transform.position) + (o.transform.lossyScale.y/2f);
+				o.transform.position = pos;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WorldMapScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class WorldMapScript : MonoBehaviour {
+
+	public GameObject loader;
+	public Slider loadingBar;
+	private AsyncOperation async;			
+	
+	
+	public void LoadScene(string scene) 
+		
+	{
+		loader.SetActive(true);
+		StartCoroutine(LoadLevelWithBar(scene));
+	}
+
+	IEnumerator LoadLevelWithBar (string sceneName)
+	{
+		async = Application.LoadLevelAsync(sceneName);
+		while (!async.isDone)
+		{
+			loadingBar.value = async.progress;
+			yield return null;
+		}
+	}
+	
+}
