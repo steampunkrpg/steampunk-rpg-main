0b8d8f2d456aa155a43a2bc91adca0e54815ed22
thehbomb34 <witt.102@buckeyemail.osu.edu>
2016-02-28 23:23:10 -0500

    Adding font change for battle
    added comic sans fonts to battle, added new animation IEnum

--- Assets/Scripts/Battle/WriteMovelist.cs
+++ Assets/Scripts/Battle/WriteMovelist.cs
@@ -7,13 +7,18 @@ public class WriteMovelist : MonoBehaviour {
     //public GameObject canvas;
     public Text attack1;
     public Text attack2;
+    Font sans;
 
 	// Use this for initialization
 	void Start () {
-        attack1.text = "Deep Six";
+        sans = Resources.Load("Comic Sans MS", typeof(Font)) as Font;
+        attack1.text = "Gritty Slap";
         attack2.text = "Riptide";
-        
-        
+        attack1.font = (Font)Resources.Load("911Fonts.com_ComicSansMSRegular__-_911fonts.com-fonts-BpCS");
+        attack2.font = (Font)Resources.Load("True Lies");
+        attack1.fontSize = 20;
+        attack2.fontSize = 20;
+        attack2.enabled = true;
     }
 	
 	// Update is called once per frame
