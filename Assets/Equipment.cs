7ffc104fad78ee2ace53eba750c510242820ba8e
nulray <jaredmheadings@gmail.com>
2016-03-02 03:14:31 -0500

    More inventory stuff. Just trying to figure out number updates, everything else should work.

---
+++ Assets/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Equipment : MonoBehaviour {
+
+	public GameObject equippedWeapon;
+	public GameObject equippedArmor;
+	public GameObject equippedHelm;
+
+}
--- Assets/Scripts/Makepeace/InventoryManager.cs
+++ Assets/Scripts/Makepeace/InventoryManager.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class InventoryManager : MonoBehaviour {
 
-	private Dictionary<string, int> itemsAndCounts;
+	private List<GameObject> buttonsInMenu;
+	public Dictionary<string, int> itemsAndCounts;
 	private Vector3 heightOffset;
 	private int buttonCount = 0, hasClicked = 0;
 	public GameObject inventoryWindow, inventoryEntry, inventoryEntryGO;
 	private Text buttonText;
 
 	void Start() {
+		buttonsInMenu = new List<GameObject> ();
 		heightOffset = new Vector3(0.0f, 80.0f, 0.0f);
 		itemsAndCounts = new Dictionary<string, int> ();
 		inventoryWindow = GameObject.Find ("InventoryWindow");
@@ -30,13 +34,17 @@ public class InventoryManager : MonoBehaviour {
 		}
 	}
 
-	public void UseItem(string item) {
-		if (itemsAndCounts.ContainsKey (item) == true) {
-			itemsAndCounts [item] -= 1;
+	//handles use of the item
+	public void UseItem(Button button)
+	{	
+		Regex rgx = new Regex("[^a-zA-Z]+");
 
-			if (itemsAndCounts [item] == 0) {
-				itemsAndCounts.Remove (item);
-			}
+		Text pressedText = button.GetComponentInChildren<Text> ();
+		string pressedTextKey = rgx.Replace (pressedText.text, "");
+
+		if (pressedTextKey == "Potion") {
+			print ("WHY IS THIS WRONG");
+			print (itemsAndCounts ["Potion"]);
 		}
 	}
 
@@ -55,14 +63,12 @@ public class InventoryManager : MonoBehaviour {
 		inventoryEntryGO.transform.parent = this.transform.FindChild ("InventoryWindow");
 		inventoryEntryGO.name = "Inventory Entry " + buttonCount;
 		inventoryEntryGO.transform.position = inventoryWindow.transform.position + heightOffset;
+		buttonsInMenu.Add (inventoryEntryGO);
 
 		buttonText = inventoryEntryGO.transform.FindChild("Text").GetComponent<Text>();
-		buttonText.text = "  " + item + "   x" + count;
-		Debug.Log (buttonText.text);
+		buttonText.text = " " + item + ": " + count;
 
 		buttonCount += 1;
 		heightOffset.y -= 40.0f;
 	}
-
-
 }
