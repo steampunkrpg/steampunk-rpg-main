ddf9b77e282b04e2e28fba12113a10bcdee26569
Caleb Underwood <Caleb Underwood>
2016-03-29 18:21:04 -0400

    Pause Menu Added

---
+++ Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu1 : MonoBehaviour {
+    
+	public void Continue_Button()
+    {
+        GameManager.instance.State = GameManager.instance.prevState;
+        this.gameObject.SetActive(false);
+    }
+
+    public void Quit_Button()
+    {
+        this.gameObject.SetActive(false);
+        GameManager.instance.LoadScene("New_Menu_Scene");
+    }
+}
