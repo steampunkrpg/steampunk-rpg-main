c6147a422661b183e1664378234b3e1bcf0d8400
nulray <jaredmheadings@gmail.com>
2016-04-06 11:14:23 -0400

    push of some new stuff for inventory/equipment use

---
+++ Assets/Scripts/Makepeace/Consumable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Consumable : MonoBehaviour
+{
+	public string cName;
+	public string statAffected;
+	public float modifier;
+
+	public Consumable(string cName, string statAffected, float modifier) {
+		this.cName = cName;
+		this.statAffected = statAffected;
+		this.modifier = modifier;
+	}
+}
--- Assets/Scripts/Makepeace/InventoryManager.cs
+++ Assets/Scripts/Makepeace/InventoryManager.cs
@@ -6,19 +6,18 @@ using UnityEngine.EventSystems;
 using System.Collections;
 
 public class InventoryManager : MonoBehaviour {
-	public static Dictionary<string, int> itemsAndCounts;
+	
+	public static List<KeyValuePair<string, int>> itemsAndCounts;
 	public GameObject inventoryWindow, inventoryEntry, inventoryEntryGO;
-	public Unit activePlayer;
 
 	private List<GameObject> buttonsInMenu;
 	private Vector3 heightOffset;
-	private int buttonCount = 0, hasClicked = 0;
 	private Text buttonText;
 
 	public void CreateDefault() {
 		buttonsInMenu = new List<GameObject> ();
 		heightOffset = new Vector3(0.0f, 80.0f, 0.0f);
-		itemsAndCounts = new Dictionary<string, int> ();
+		itemsAndCounts = new List<KeyValuePair<string, int>> ();
 		inventoryWindow = GameObject.Find ("InventoryWindow");
 		//inventoryWindow.SetActive (false);
 
@@ -27,19 +26,37 @@ public class InventoryManager : MonoBehaviour {
 		AddItem ("Sword", 1);
 
 		//generate some "weapons" for testing
-		Weapon WoodSword = new Weapon("WoodSword", 5, 95, 20, 5, new List<float>{1}, 0);
+		/*Weapon WoodSword = new Weapon("WoodSword", 5, 95, 20, 5, new List<float>{1}, 0);
 		Weapon Crossbow = new Weapon ("Crossbow", 7, 90, 20, 3, new List<float> {2}, 0);
 		Weapon PyroFlame = new Weapon ("PyroFlame", 9, 70, 10, 1, new List<float> {1, 2, 3}, 1);
-		Weapon HealingHand = new Weapon ("HealingHand", 3, 100, 10, 1, new List<float> {1}, -1);
+		Weapon HealingHand = new Weapon ("HealingHand", 3, 100, 10, 1, new List<float> {1}, -1);*/
 	}
 
 	public void AddItem(string item, int count) {
-		if (itemsAndCounts.ContainsKey (item) == false) {
-			itemsAndCounts.Add (item, count);
-			ButtonCreator (item, count);
-		} else {
-			itemsAndCounts [item] += 1;
+		
+		for (int i = 0; i < itemsAndCounts.Count; i++) {
+			if (itemsAndCounts [i].Key == item) {
+				itemsAndCounts [i] = new KeyValuePair<string, int> (item, itemsAndCounts [i].Value + 1);
+				GameObject.Destroy (GameObject.Find (item));
+				ButtonCreator (item, itemsAndCounts [i].Value + 1);
+				i = itemsAndCounts.Count;
+			} else if (i == itemsAndCounts.Count - 1 && itemsAndCounts [i].Key != item) {
+				itemsAndCounts.Add (new KeyValuePair<string, int> (item, count));
+				ButtonCreator (item, count);
+			}
 		}
+	}
+
+	void ButtonCreator (string item, int value) {
+		inventoryEntryGO = Instantiate (inventoryEntry);
+		inventoryEntryGO.transform.SetParent (inventoryWindow.transform);
+		inventoryEntryGO.name = item;
+		inventoryEntryGO.transform.position = inventoryWindow.transform.position + heightOffset;
+		buttonsInMenu.Add (inventoryEntryGO);
+		
+		buttonText = inventoryEntryGO.transform.FindChild ("Text").GetComponent<Text> ();
+		buttonText.text = " " + item + ": " + value;
+		heightOffset.y -= 40.0f;
 	}
 
 	//handles use of the item
@@ -47,13 +64,15 @@ public class InventoryManager : MonoBehaviour {
 	{	
 		/*SECTION 1*/
 		//decrements the count on the inventory button
-		Regex rgx = new Regex("[^a-zA-Z]+");
-		Text pressedText = button.GetComponentInChildren<Text> ();
-		string pressedTextKey = rgx.Replace (pressedText.text, "");
+		string pressedString = button.transform.name.ToString();
 
-		if (itemsAndCounts [pressedTextKey] != 0) {
-			itemsAndCounts [pressedTextKey] -= 1;
-			pressedText.text = " " + pressedTextKey + ": " + itemsAndCounts [pressedTextKey];
+		for (int i = 0; i < itemsAndCounts.Count; i++) {
+			if (itemsAndCounts [i].Key == pressedString) {
+				itemsAndCounts [i] = new KeyValuePair<string, int> (pressedString, itemsAndCounts [i].Value + 1);
+				GameObject.Destroy (GameObject.Find (pressedString));
+				ButtonCreator (pressedString, itemsAndCounts [i].Value - 1);
+				i = itemsAndCounts.Count;
+			}
 		}
 
 		/*SECTION 2*/
@@ -61,32 +80,4 @@ public class InventoryManager : MonoBehaviour {
 		var apsScript =	GameManager.instance.activePlayer.GetComponent<Stats> ();
 		var apwScript = GameManager.instance.activeEnemy.GetComponent<Weapon> ();
 
-		switch (pressedTextKey) {
-		case "Potion":
-			apsScript.cHP += apsScript.mHP * 0.40f;
-			break;
-		case "Sword":
-			apwScript.Mt = 10;
-			apwScript.Hit = 95;
-			apwScript.Crit = 15;
-			apwScript.Wt = 3;
-			apwScript.Rng = new List<float> { 1, 1 };
-			apwScript.type = 0;
-			break;
-			}
-		}
-
-	void ButtonCreator (string item, int count) {
-		inventoryEntryGO = Instantiate (inventoryEntry);
-		inventoryEntryGO.transform.SetParent(inventoryWindow.transform);
-		inventoryEntryGO.name = "Inventory Entry " + buttonCount;
-		inventoryEntryGO.transform.position = inventoryWindow.transform.position + heightOffset;
-		buttonsInMenu.Add (inventoryEntryGO);
-
-		buttonText = inventoryEntryGO.transform.FindChild("Text").GetComponent<Text>();
-		buttonText.text = " " + item + ": " + count;
-
-		buttonCount += 1;
-		heightOffset.y -= 40.0f;
-	}
 }
