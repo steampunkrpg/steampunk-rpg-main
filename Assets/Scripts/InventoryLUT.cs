7d6fbe86cbd329e96ac70246573d2b3e0892c821
nulray <jaredmheadings@gmail.com>
2016-04-12 23:01:56 -0400

    change to look-up table for items (switch case!)

--- Assets/Scripts/InventoryLUT.cs
+++ Assets/Scripts/InventoryLUT.cs
@@ -5,28 +5,41 @@ using System;
 
 public class InventoryLUT : MonoBehaviour {
 
-	public List<Item> treasury;
-	//public List<Consumable> medicineCabinet;
-
-	void Start () {
-		Item WoodSword = new Item(1, "WoodSword", 5, 95, 20, 5, new List<float>{1}, 0);
-		Item Crossbow = new Item (1, "Crossbow", 7, 90, 20, 3, new List<float> {2}, 0);
-		Item PyroFlame = new Item (1, "PyroFlame", 9, 70, 10, 1, new List<float> {1, 2, 3}, 1);
-		Item HealingHand = new Item (1, "HealingHand", 3, 100, 10, 1, new List<float> {1}, -1);
-
-		Item Potion = new Item (0, "Potion");
-		Item FuryElixir = new Item (0, "FuryElixir");
-
-		treasury = new List<Item> {WoodSword, Crossbow, PyroFlame, HealingHand, Potion, FuryElixir};
-	}
+	public Item returnedItem;
 
 	public Item Lookup (string used) {
-		for (int i = 0; i < treasury.Count; i++) {
-			if (treasury [i].ToString() == used) {
-				Item itemToUse = treasury [i];
-				return itemToUse;
-			}
+
+		switch (used) {
+
+		case "WoodSword":
+			returnedItem = new Item (1, "WoodSword", 5, 95, 20, 5, new List<float>{ 1 }, 0);
+			break;
+
+		case "Crossbow":
+			returnedItem = new Item (1, "Crossbow", 7, 90, 20, 3, new List<float> { 2 }, 0);
+			break;
+
+		case "PyroFlame":
+			returnedItem = new Item (1, "PyroFlame", 9, 70, 10, 1, new List<float> { 1, 2, 3 }, 1);
+			break;
+
+		case "HealingHand":
+			returnedItem = new Item (1, "HealingHand", 3, 100, 10, 1, new List<float> { 1 }, -1);
+			break;
+
+		case "Potion":
+			returnedItem = new Item (0, "Potion");
+			break;
+
+		case "FuryElixir":
+			returnedItem = new Item (0, "FuryElixir");
+			break;
+
+		default:
+			returnedItem = null;
+			break;
 		}
-		return null;
+
+		return returnedItem;
 	}
 }
