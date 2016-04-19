18ebe45aa53be705f345152b844b0f28294fce31
nulray <jaredmheadings@gmail.com>
2016-04-19 11:35:37 -0400

    initial music stuff

---
+++ Assets/Scripts/Audio Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private AudioSource musicPlayer;
+	private AudioClip clip;
+	private GameObject musicSource, musicSourceGO;
+	private Scene scene;
+	private string sceneName;
+
+	void PlayMusic () {
+		scene = SceneManager.GetActiveScene ();
+		sceneName = scene.name;
+
+		musicSource = (GameObject)Resources.Load ("Prefabs/MusicPlayer", typeof(GameObject));
+		musicSourceGO = Instantiate (musicSource) as GameObject;
+		musicSourceGO.transform.parent = GameObject.Find ("GameManager(Clone)").transform;
+		musicPlayer = musicSourceGO.GetComponent<AudioSource> ();
+
+		/*
+		switch (sceneName){
+		case "New_Menu_Scene":
+			clip = 
+			playedMusic.clip = 
+		
+		}
+		*/
+	}
+}
