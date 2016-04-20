e4ab4893d59dd860d6c7d5c474d3f0edcab74833
alquist2 <mikea12122@gmail.com>
2016-04-19 23:30:52 -0400

    Story UI working with Tutorial level.

--- Assets/Scripts/Alquist/StoryUI.cs
+++ Assets/Scripts/Alquist/StoryUI.cs
@@ -4,19 +4,23 @@ using UnityEngine.UI;
 
 public class StoryUI : MonoBehaviour {
 
+    public bool storyContinue;
+
 	// Use this for initialization
 	void Start () {
-	
+        this.gameObject.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (storyContinue)
+        {
+            this.gameObject.SetActive(false);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            storyContinue = true;
+        }
 	}
-
-    void UpdateUI ()
-    {
-        
-    }
 
 }
