3bc2f3f98af71324eb2f2cb04aeceddd575a385b
thehbomb34 <witt.102@buckeyemail.osu.edu>
2016-04-24 14:30:32 -0400

    Finished Intro Scene / integrated with current game
    Introduction scene in between start menu and world map. Had fun!

---
+++ Assets/Scripts/Camera/CameraIntroScene.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraIntroScene : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera camera2;
+    public Camera camera3;
+    public Camera camera4;
+    public bool atStart;
+    public bool onCam2;
+    public bool onMainCam2;
+    public bool onCam3;
+    public bool onCam4;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera.enabled = true;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
+        atStart = true;
+        onCam2 = false;
+        onCam3 = false;
+        onCam4 = false;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (atStart)
+        {
+            mainCamera.transform.Translate(0.0f, 0.0f, 0.01f);
+            if (mainCamera.transform.position.z >= -1)
+            {
+                atStart = false;
+                onCam2 = true;
+                mainCamera.enabled = false;
+                camera2.enabled = true;
+            }
+        }
+        else if (onCam2)
+        {
+            camera2.transform.Translate(-0.01f, 0.0f, 0.0f);
+            if (camera2.transform.position.x <= -1.4)
+            {
+                onCam2 = false;
+                camera2.enabled = false;
+                mainCamera.enabled = true;
+                onMainCam2 = true;
+            }
+        }
+        else if (onMainCam2)
+        {
+            mainCamera.transform.Translate(0.0f, 0.0f, -.01f);
+            if (mainCamera.transform.position.z <= -5)
+            {
+                onMainCam2 = false;
+                onCam3 = true;
+                mainCamera.enabled = false;
+                camera3.enabled = true;
+            }
+        }
+        else if (onCam3)
+        {
+            camera3.transform.Translate(0.0f, -.005f, 0.0f);
+            if (camera3.transform.position.y <= .3)
+            {
+                onCam4 = true;
+                onCam3 = false;
+                camera3.enabled = false;
+                camera4.enabled = true;
+            }
+        }
+        else if (onCam4)
+        {
+            camera4.transform.Translate(0.0f, 0.0f, -.001f);   
+        }
+	}
+}
--- Assets/Scripts/WriteText.cs
+++ Assets/Scripts/WriteText.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WriteText : MonoBehaviour {
 
@@ -9,19 +10,27 @@ public class WriteText : MonoBehaviour {
     public GameObject sound1;
     public GameObject sound2;
     public GameObject sound3;
+    public GameObject sound4;
+    public GameObject sound5;
     public bool writing1;
     public bool writing2;
     public bool writing3;
+    public bool writing4;
+    public bool writing5;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         writeText.text = "";
         writing1 = true;
         StartCoroutine(AnimateText());
         writing2 = false;
-        writing2 = false;
+        writing3 = false;
+        writing4 = false;
+        writing5 = false;
         sound2.GetComponent<AudioSource>().Pause();
         sound3.GetComponent<AudioSource>().Pause();
+        sound4.GetComponent<AudioSource>().Pause();
+        sound5.GetComponent<AudioSource>().Pause();
 	}
 	
     public IEnumerator AnimateText()
@@ -34,7 +43,7 @@ public class WriteText : MonoBehaviour {
         {
             str += text[i++];
             writeText.text = str;
-            yield return new WaitForSeconds(.08f);
+            yield return new WaitForSeconds(.065f);
         }
     }
 
@@ -42,13 +51,13 @@ public class WriteText : MonoBehaviour {
     {
         sound2.GetComponent<AudioSource>().Play();
         int i = 0;
-        text = "Basically everyone was like \"Ey Yo fam if we were to quantify our happiness at this instant, it would probably be low\"";
+        text = "Basically everyone was like Yo fam if we were to quantify our happiness at this instant in time, it would probably be low";
         string str = "";
         while (i < text.Length && writing2)
         {
             str += text[i++];
             writeText.text = str;
-            yield return new WaitForSeconds(.08f);
+            yield return new WaitForSeconds(.065f);
         }
     }
 
@@ -62,7 +71,67 @@ public class WriteText : MonoBehaviour {
         {
             str += text[i++];
             writeText.text = str;
-            yield return new WaitForSeconds(.08f);
+            yield return new WaitForSeconds(.065f);
+        }
+    }
+
+    public IEnumerator textPt4()
+    {
+        sound4.GetComponent<AudioSource>().Play();
+        int i = 0;
+        text = "I should prolly introduce myself. Roughly translated to English, my name is \"Ungrqhqkhkhkhkhqkhahhah\" *coughs* Oh oh lawdy, oh lawdy that name is, ugh, that name is AWFUL, let's do another take. Wait, wha-we aren't doing another take? We're still recording? Ugh Jeepers Creepers I suck.";
+        string str = "";
+        while (i < text.Length && writing4)
+        {
+            str += text[i++];
+            writeText.text = str;
+            yield return new WaitForSeconds(.087f);
+            if (str.Equals("I should prolly introduce myself. Roughly translated to English, my name is \"Ungrqhqkhkhkhkhqkhahhah\" *coughs*")) {
+                yield return new WaitForSeconds(1.2f);
+                writeText.text = "";
+                str = "";
+            }
+            if (str.Equals(" Oh oh lawdy, oh lawdy that name is, ugh, that name is AWFUL, let's do another take. Wait, wha-we aren't doing"))
+            {
+                yield return new WaitForSeconds(.65f);
+                writeText.text = "";
+                str = "";
+            }
+        }
+    }
+
+    public IEnumerator textPt5()
+    {
+        sound5.GetComponent<AudioSource>().Play();
+        int i = 0;
+        text = "In short, I was tasked by some dudes with going back in time to try to beat this guy before he becomes the Warlord, & his influence spreads like a oil spill. If I succeed, we can return to the present & all will be fine and dandy. But if I fail, it will not just be famine today...but Famine Tomorrow. (Eh? You like that? Usin' the title of the game and such?)";
+        string str = "";
+        while (i < text.Length && writing5)
+        {
+            str += text[i++];
+            writeText.text = str;
+            yield return new WaitForSeconds(.055f);
+            if (str.Equals("In short, I was tasked by some dudes with going back in time to try to beat this guy before he becomes the Warlord, &"))
+            {
+                yield return new WaitForSeconds(.2f);
+                writeText.text = "";
+                str = "";
+            }
+            else if (str.Equals(" his influence spreads like a oil spill. If I succeed, we can return to the present & all will be fine and dandy. But if"))
+            {
+                yield return new WaitForSeconds(.2f);
+                writeText.text = "";
+                str = "";
+            }
+        }
+    }
+
+    IEnumerator LoadLevelWithBar(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        while (!async.isDone)
+        {
+            yield return null;
         }
     }
 
@@ -84,5 +153,26 @@ public class WriteText : MonoBehaviour {
             writeText.text = "";
             StartCoroutine(textPt3());
         }
-	}
+        else if (Input.GetKeyDown(KeyCode.Return) && writing3)
+        {
+            sound3.GetComponent<AudioSource>().Stop();
+            writing3 = false;
+            writing4 = true;
+            writeText.text = "";
+            StartCoroutine(textPt4());
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && writing4)
+        {
+            sound4.GetComponent<AudioSource>().Stop();
+            writing4 = false;
+            writing5 = true;
+            writeText.text = "";
+            StartCoroutine(textPt5());
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && writing5)
+        {
+            sound5.GetComponent<AudioSource>().Stop();
+            StartCoroutine(LoadLevelWithBar("World_Map"));
+        }
+    }
 }
