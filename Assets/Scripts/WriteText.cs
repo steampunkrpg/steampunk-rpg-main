e486ce3ffa9bcd84c1d180b0d209bd8b59621278
thehbomb34 <witt.102@buckeyemail.osu.edu>
2016-04-24 04:11:31 -0400

    Fixed bugs with Command Text not locking to bottom left corner of screen; began construction of intro scene
    -Pretty self-explanatory, but now the text will always appear in right
    spot

---
+++ Assets/Scripts/WriteText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WriteText : MonoBehaviour {
+
+    private string text;
+    public Text writeText;
+
+	// Use this for initialization
+	void Start () {
+        writeText.text = "";
+        StartCoroutine(AnimateText());
+	}
+	
+    public IEnumerator AnimateText()
+    {
+        int i = 0;
+        text = "Imma come clean, I have no idea how we got to this point. The regime of the warlord can be summed up like this:";
+        string str = "";
+        while (i < text.Length)
+        {
+            str += text[i++];
+            writeText.text = str;
+            yield return new WaitForSeconds(.05f);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
