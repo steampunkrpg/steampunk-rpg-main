4b39901c90dc59c472485caa92246af031f0fb97
nulray <jaredmheadings@gmail.com>
2016-04-23 22:39:58 -0400

    added music controller script

---
+++ Assets/Scripts/Audio Scripts/MusicController.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+private static var instance;
+
+ public static function GetInstance() {
+     return instance;
+ }
+  
+ function Awake() {
+     if (instance != null && instance != this) {
+         Destroy(this.gameObject);
+         return;
+     } else {
+         instance = this;
+     }
+     DontDestroyOnLoad(this.gameObject);
+ }
